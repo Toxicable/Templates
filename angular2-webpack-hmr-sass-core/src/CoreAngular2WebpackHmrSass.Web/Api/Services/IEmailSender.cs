4fe62f3faa3ef2ca7f0c228773d4eadc6c2ebbc2
Fabian <Fabian@DESKTOP-0MBDLK1>
2016-10-04 09:41:58 +1300

    removed old angular project

--- angular2-webpack-hmr-sass-core/src/CoreAngular2WebpackHmrSass.Web/Api/Services/IEmailSender.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace AppName.Web.Services {
-    public interface IEmailSender {
-        Task SendEmailAsync(string email, string subject, string message);
-    }
-}
--- angular2-webpack-hmr-sass-core/src/CoreAngular2WebpackHmrSass.Web/App/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-﻿import { provideRouter, RouterConfig } from '@angular/router';
-import { HomeComponent } from './home';
-
-export const appRoutes: RouterConfig = [
-    { path: '', component: HomeComponent },
-    { path: '**', redirectTo: '' }
-];
-
-export const APP_ROUTER_PROVIDER = provideRouter(appRoutes);
