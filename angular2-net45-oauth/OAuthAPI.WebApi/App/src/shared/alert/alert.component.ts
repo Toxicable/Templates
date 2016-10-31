be0d0793380d911e868d3c38a622991071333b9e
Fabian Wiles <fabianwiles@live.com>
2016-11-01 11:39:51 +1300

    added in stae managed alerts

--- angular2-net45-oauth/OAuthAPI.WebApi/App/src/shared/alert/alert.component.ts
+++ angular2-net45-oauth/OAuthAPI.WebApi/App/src/shared/alert/alert.component.ts
@@ -1,39 +1,23 @@
-import {Component, OnInit, OnDestroy} from '@angular/core'
-import {AlertService} from "../../core/services/alert.service";
-import {Subscription} from "rxjs";
-import {Alert} from "../../core/models/alert.model";
-import {AlertType} from "../../core/models/alert-types";
+import { Component, OnInit, OnDestroy} from '@angular/core'
+import { AlertService} from "../../core/services/alert.service";
+import { Observable} from "rxjs/Observable";
+import { Alert} from "../../core/models/alert.model";
+import { AppState} from '../../app/app-store';
+import { Store} from '@ngrx/store';
 
 @Component({
     selector: 'alert',
     templateUrl: 'alert.component.html',
     styleUrls: ['alert.component.scss']
 })
-export class AlertComponent implements OnInit, OnDestroy{
-    constructor(private alertService: AlertService){}
-
-    subscription: Subscription;
-    alerts: Alert[]= [];
-    id: number = 1;
-
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
-    }
+export class AlertComponent implements OnInit{
+    constructor(private alertService: AlertService,
+                private store: Store<AppState>
+    ){}
+    alerts: Observable<Alert[]>;
 
     ngOnInit(): void {
-        this.subscription = this.alertService.alertAnnounced$.subscribe((alert) => {
-                alert.id = this.id;
-                this.id ++;
-
-                this.alerts.push(alert);
-                setTimeout(() =>{
-                    let index = this.alerts.findIndex( a => a.id == this.id - 1);
-                    this.alerts.splice(index, 1);
-                    this.id --;
-
-                }, 5000);
-            });
+        this.alerts = this.store.select( state => state.alerts);
     }
-
 
 }
