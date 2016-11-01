a3331d2660d4f4e5ef5df3bb751c35f1859f186b
Fabian Wiles <fabianwiles@live.com>
2016-11-01 17:02:13 +1300

    white space

--- angular2-net45-oauth/OAuthAPI.WebApi/App/src/app/app-store.ts
+++ angular2-net45-oauth/OAuthAPI.WebApi/App/src/app/app-store.ts
@@ -1,16 +1,16 @@
-import {StoreModule} from '@ngrx/store';
-import {Tokens} from '../core/models/tokens';
-
-import {User} from '../+admin/models/user';
-import {Alert} from '../core/models/alert.model';
-import {ProfileModel} from '../core/models/profile-model';
-
-import {tokensReducer} from '../core/auth/token.reducer';
-import {alertsReducer} from '../shared/alert/alert.reducer';
-import {loadingBarReducer} from '../shared/loading-bar/loading-bar.reducer';
-import {usersReducer} from '../+admin/users/user-reducer';
-import {profileReducer} from '../core/profile/profile.reducers';
-import {loggedInReducer} from '../core/auth/loggedIn.reducers';
+import { StoreModule }       from '@ngrx/store';
+import { Tokens }            from '../core/models/tokens';
+//models
+import { User }              from '../+admin/models/user';
+import { Alert }             from '../core/models/alert.model';
+import { ProfileModel }      from '../core/models/profile-model';
+//reducers
+import { tokensReducer }     from '../core/auth/token.reducer';
+import { alertsReducer }     from '../shared/alert/alert.reducer';
+import { loadingBarReducer } from '../shared/loading-bar/loading-bar.reducer';
+import { usersReducer }      from '../+admin/users/user-reducer';
+import { profileReducer }    from '../core/profile/profile.reducers';
+import { loggedInReducer }   from '../core/auth/loggedIn.reducers';
 
 export interface AppState{
     users: User[],
