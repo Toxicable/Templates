93812e4538e3932256fee5fc048c285e16f5ba8f
Fabian Wiles <fabianwiles@live.com>
2016-11-06 10:53:34 +1300

    added a sample test

--- angular2-net45-oauth/OAuthAPI.WebApi/App/config/webpack.test.js
+++ angular2-net45-oauth/OAuthAPI.WebApi/App/config/webpack.test.js
@@ -31,7 +31,8 @@ module.exports = {
                 test: /\.css$/,
                 include: helpers.root('src', 'app'),
                 loader: 'raw'
-            }
+            },
+            { test: /\.scss$/, exclude: /node_modules/, loader: 'raw-loader!sass-loader' }
         ]
     }
 }
